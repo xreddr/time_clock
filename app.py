0c97b97f3286a955866a9e27c14ce32cf981a2b7
xreddr <redwardhenion@gmail.com>
2023-11-19 17:11:55 -0500

    further style

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 # App foundation
 import kivy
 kivy.require('2.2.1')
-from kivy.app import App
+from kivy.config import Config
+# Config.set('graphics', 'fullscreen', 1)
+
+# from kivy.app import App
+from kivymd.app import MDApp
+from kivymd.uix.behaviors.toggle_behavior import MDToggleButton
+from kivymd.uix.button import MDRoundFlatIconButton
 import time
 # Widget elements
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
+# from kivy.uix.gridlayout import GridLayout
+# from kivy.uix.label import Label
+# from kivy.uix.textinput import TextInput
 from kivy.core.window import Window
 from kivy.clock import Clock
 from kivy.lang import Builder
@@ -15,55 +21,27 @@ from kivy.uix.widget import Widget
 Builder.load_file('format.kv')
 
 # Window size
-Window.size = (800, 480)
+# Window.size = (800, 480)
+Window.fullscreen = True
 
-
-
-    
-# class myclock(Label):
-#     def update(self, *args):
-#         structtime_obtain = time.localtime()
-#         strftime_output = time.strftime("%H:%M:%S", structtime_obtain)
-#         self.text = strftime_output
-
-# class TimeDisplay(GridLayout):
-#     def __init__(self, **kwargs):
-#         super(TimeDisplay, self).__init__(**kwargs)
-#         self.cols = 2
-#         self.clock1 = myclock()
-#         Clock.schedule_interval(self.clock1.update, 1)
-#         self.add_widget(self.clock1)
-
-# class LoginScreen(GridLayout):
-
-#     def __init__(self, **kwargs):
-#         super(LoginScreen, self).__init__(**kwargs)
-#         self.cols = 2
-#         self.add_widget(Label(text='User Name'))
-#         self.username = TextInput(multiline=False)
-#         self.add_widget(self.username)
-#         self.add_widget(Label(text='password'))
-#         self.password = TextInput(password=True, multiline=False)
-#         self.add_widget(self.password)
-#         self.clock1 = myclock()
-#         Clock.schedule_interval(self.clock1.update, 1)
-#         self.add_widget(self.clock1)
 
 class MainDisplay(Widget):
-    def change_gif(self, file):
+    def change_gif(self, file, color):
         self.ids.gif.source = file
-    pass
-    # time = TimeDisplay()
-    # def clock(self):
-    #     structtime_obtain = time.localtime()
-    #     strftime_output = time.strftime("%H:%M:%S", structtime_obtain)
-    #     self.ids.clock_time.text = strftime_output
-    #     self.call_time()
+        self.ids.clock_time.color = color
 
-    # def call_time(self):
-    #     Clock.schedule_interval(self.clock(), 1)
+    def act1(self):
+        Clock.schedule_interval(self.timer1, 0.1)
 
-class MyApp(App):
+    def timer1(self, *args):
+        start = time.time()
+        elapsed = time.time() - start
+        print(elapsed)
+        self.ids.act1.text = str(elapsed)
+        
+
+
+class MyApp(MDApp):
     def on_start(self):
         Clock.schedule_interval(self.update_clock, 1)
 
